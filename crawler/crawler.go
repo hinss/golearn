86c2f2b86be9d24b94fee50ff87cc960594da4dc
wyman <277439189@qq.com>
2021-02-27 16:09:09 +0800

    单机版爬虫: 1.1 从页面前1024个字节推断出字符集 做好编码转换reader

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -20,10 +25,26 @@ func main() {
 		return
 	}
 
-	bytes, err := ioutil.ReadAll(resp.Body)
+	// 获取推断的类型
+	encoding := determineEncoding(resp.Body)
+
+	// 解决不同http页面的编码问题
+	utf8Reader := transform.NewReader(resp.Body, encoding.NewDecoder())
+	bytes, err := ioutil.ReadAll(utf8Reader)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s\n", bytes)
+}
 
+/**
+从网页的前1024个bytes中推断 页面编码
+*/
+func determineEncoding(reader io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(reader).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module learngo
 
 go 1.14
+
+require (
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+	golang.org/x/text v0.3.5
+)

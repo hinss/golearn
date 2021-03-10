e05ca0873b850fdab72f14a7f5deff55b39dfeab
wyman <277439189@qq.com>
2021-03-10 08:14:12 +0800

    go实现快速排序 基础版

---
+++ algorithm/quicksort.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func quickSort(arr []int) []int {
+
+	minIndex := 0
+	var hasSwapped bool
+	for i := 0; i < len(arr); i++ {
+
+		hasSwapped = false
+		for j := i; j < len(arr)-1; j++ {
+			if arr[j+1] < arr[j] {
+				minIndex = j + 1
+				hasSwapped = true
+			}
+		}
+
+		if !hasSwapped {
+			return arr
+		}
+
+		// swap
+		temp := arr[minIndex]
+		arr[minIndex] = arr[i]
+		arr[i] = temp
+
+		fmt.Println(arr)
+	}
+
+	return arr
+}
+
+func main() {
+
+	arr := []int{6, 5, 4, 3, 2, 1}
+	arr = quickSort(arr)
+	fmt.Printf("final arr: %v", arr)
+}

09ce41e7357c892e03a110e6ffbf337327c2166b
hins <277439189@qq.com>
2021-09-21 19:41:11 +0800

    cgo简单实例

---
+++ cgo/main.go
@@ -0,0 +1,10 @@
+package main
+
+// #include "test.h"
+// #cgo LDFLAGS: libtest.a -lstdc++
+import "C"
+
+func main() {
+
+	C.test()
+}
---
+++ cgo/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+extern "C" {
+
+    void test()
+    {
+        cout << "this is a test" << endl;
+    }
+
+}
